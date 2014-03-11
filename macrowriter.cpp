884f3db577e2f330b42cd26a57acac62b4185074
Jonathan Clapson <jonathan@localhost.localdomain>
2014-03-12 10:42:02 +1300

    add macrowriter functions

---
+++ macrowriter.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+FILE *fp;
+char *name;
+
+int macro_writer_init(const char *file_name)
+{
+	fp = fopen(file_name, "w");
+	if (!fp)
+		return -1;
+		
+	name = (char *)file_name;
+	printf("Macro file %s open\n", name);
+	return 0;
+}
+
+int macro_writer_write_line(int x0, int y0, int x1, int y1)
+{
+	if (!fp)
+		return -1;
+	
+	fprintf(fp, "Line;500;1000000000;0;False;27500000;12500050;Disabled;10000;2\n");
+	
+	return 0;
+}
+
+int macro_writer_write_arc()
+{
+	if (!fp)
+		return -1;
+		
+	fprintf(fp, "arc\n");
+	return 0;
+}
+
+int macro_writer_close()
+{
+	fclose(fp);
+	printf("Macro file %s closed\n", name);
+	name = (char *)"";
+	return 0;
+}
---
+++ macrowriter.h
@@ -0,0 +1,12 @@
+#ifndef __MACRO_WRITER_H__
+#define __MACRO_WRITER_H__
+
+int macro_writer_init(const char *file_name);
+
+int macro_writer_write_line(int x0, int y0, int x1, int y1);
+
+int macro_writer_write_arc();
+
+int macro_writer_close();
+
+#endif /* __MACRO_WRITER_H__ */
